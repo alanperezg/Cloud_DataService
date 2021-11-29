6284321f029def5c483b3e0d897a553494f4954e
Alan Pérez <alan@perezgarcia.mx>
2021-11-29 02:14:08 -0600

    Fixed query error

--- utils/providerAccountActions.js
+++ utils/providerAccountActions.js
@@ -4,7 +4,7 @@ const { logError } = require('./logger')
 const getAccountByAuth = async ({user, password}) => {
     const DB = new DBConnection();
     try{
-        const data = await DB.query('SELECT id, firstName, lastName, pp FROM ProvidertUser WHERE user = ? AND password = ?', [user, password])
+        const data = await DB.query('SELECT id, firstName, lastName, pp FROM ProviderUser WHERE user = ? AND password = ?', [user, password])
         DB.close();
         return data[0]
     }catch(e){
